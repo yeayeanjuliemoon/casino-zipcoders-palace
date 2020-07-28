11ecc9778f397cd71dc144ac14a311ed63b188a7
Frank Hulmes <fdhulmes@yahoo.com>
2020-07-28 19:18:29 -0400

    Dice and Dicegame structure added

---
+++ src/main/java/io/zipcoder/casino/CrapsGame.java
@@ -0,0 +1,13 @@
+package io.zipcoder.casino;
+
+public class CrapsGame extends DiceGame {
+    private Map<Player, Wager> playerWagerMap;
+    private Integer roundNum;
+    private Integer point;
+
+    public CrapsGame(){
+        super();
+    }
+
+
+}
---
+++ src/main/java/io/zipcoder/casino/DiceGame.java
@@ -0,0 +1,15 @@
+package io.zipcoder.casino;
+
+public abstract class DiceGame {
+    Dice dice;
+    Integer numberRoll;
+    Integer diceSum;
+
+    public DiceGame(){
+
+    }
+
+    public String printDiceValues(){
+        return "";
+    }
+}
