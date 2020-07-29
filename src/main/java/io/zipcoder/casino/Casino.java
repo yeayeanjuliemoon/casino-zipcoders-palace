9fe36e2bcb28a8d1ff44114b32350b0898283ee5
George Ionas <gionas@zipcoders-MacBook-Pro.local>
2020-07-29 10:59:57 -0400

    implementing menu and choice methods

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -1,11 +1,12 @@
 package io.zipcoder.casino;
 
-import java.util.ArrayList;
+import io.zipcoder.casino.card.games.Blackjack;
+import io.zipcoder.casino.card.games.GoFish;
+
 
 public class Casino {
 
-    Player activePlayer;
-    //List<Player> Active Players
+    public Player activePlayer;
     public Game currentGame;
     public Console console;
     private boolean opFlag;
@@ -31,18 +32,28 @@ public class Casino {
     }
 
 
-    public Game selectGame() {
-        return Game;
-
+    public void selectGame(Game selectedGame) {
+        this.currentGame = selectedGame;
+        playGame();
     }
 
 
-    public void playGame() {
-
+    private void playGame() {
+        currentGame.play();
     }
 
     public String printCasinoMenu() {
-        return "A casino menu";
+        String menu = "******* ♖ ZipCoder's Palace ♖  *******\n" +
+                      "******* Please Enter A Number: *******\n"+
+                      "* 1: Login ~~~~~~~~~~~~~~~~~~~~~~~ *\n"+
+                      "* 2: Play GoFish ~~~~~~~~~~~~~~~~~ *\n"+
+                      "* 3: Play BlackJack ~~~~~~~~~~~~~~ *\n"+
+                      "* 4: Play Craps ~~~~~~~~~~~~~~~~~~ *\n"+
+                      "* 5: Play CeeLo ~~~~~~~~~~~~~~~~~~ *\n"+
+                      "* 6: Logout ~~~~~~~~~~~~~~~~~~~~~~ *\n"+
+                      "* 7: Leave ZipCoder's Palace ~~~~~ *\n\n"+
+                      "**** You must be logged in to play a game! ****\n\n";
+        return menu;
     }
 
     public void playerLogin(String givenName) {
@@ -60,43 +71,63 @@ public class Casino {
     }
 
     public void parseMenuInput() {
-        String input = console.getStringInput("Please enter selection >");
+        Integer input = console.getIntegerInput("Selection >");
         switch (input){
-            case "Create User":
+            case 1: //Login
                 //Player player = createPlayer();
                 // activePlayer = player;
                 break;
-            case "GoFish":
-                //something
-                break;
-            case "BlackJack":
+            case 2: //GoFish
                 if(checkIfActivePlayers()){
-                    this.currentGame = BlackJack;
-                    playGame(this.currentGame);
+                    selectGame(new GoFish());
                 } else {
-                    //Error message?
+                    console.println("No player currently logged in");
                 }
                 break;
-            case "Craps":
-                //something
+            case 3: //BlackJack
+                if(checkIfActivePlayers()){
+                    selectGame(new Blackjack());
+                } else {
+                    console.println("No player currently logged in");
+                }
                 break;
-            case "Ceelo":
-                //something
+            case 4: // Craps
+                if(checkIfActivePlayers()){
+                    selectGame(new CrapsGame());
+                } else {
+                    console.println("No player currently logged in");
+                }
+                break;
+            case 5: // CeeLo
+                if(checkIfActivePlayers()){
+                    selectGame(new CeeLoGame());
+                } else {
+                    console.println("No player currently logged in");
+                }
+                break;
+            case 6: // Logout
+                if(checkIfActivePlayers()){
+                    String aPlayer = this.activePlayer.toString();
+                    playerLogout(aPlayer); // Needs to be re-looked at
+                } else {
+                    console.println("No player currently logged in");
+                }
+                break;
+            case 7: // Quit
+                quit();
                 break;
             default:
-                console.println("Please enter a valid input");
+                    console.println("Please enter a valid selection (1 -> 9)");
                 break;
-
-
         }
-
     }
 
     public Boolean checkIfActivePlayers() {
-        return null;
+        return true;
     }
 
     public void quit() {
+        console.println(" Thank you for playing at ZipCoder's Casino!");
         System.exit(0);
     }
 }
