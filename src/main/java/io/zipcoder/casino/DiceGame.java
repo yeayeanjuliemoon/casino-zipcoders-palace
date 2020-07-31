f3676f752446a9280611a08c486f99efa01896f1
Julie Moon <juliemoon95@gmail.com>
2020-07-30 23:57:22 -0400

    solving conflicts

--- src/main/java/io/zipcoder/casino/DiceGame.java
+++ src/main/java/io/zipcoder/casino/DiceGame.java
@@ -7,17 +7,12 @@ public abstract class DiceGame implements Game, GamblingGame {
     Integer numberRoll;
     Integer diceSum;
 
-public abstract class DiceGame implements Game {
-    protected Dice dice;
-    protected Integer numberRoll;
-    protected Integer diceSum;
-
     public DiceGame(Integer numDice){
         this.dice = new Dice(numDice);
     }
 
     public String printDiceValues(){
-        List<Integer> diceList = dice.getDieValues();
+        List<Integer> diceList = dice.getDiceValues();
         StringBuilder sb = new StringBuilder();
         for(Integer i : diceList){
             sb.append("[ " + i.toString() + " ]");
