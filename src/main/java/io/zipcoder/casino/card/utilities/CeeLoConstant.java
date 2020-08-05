1a814674325e40615ebde12d6a6874e79fa8bcf7
Julie Moon <juliemoon95@gmail.com>
2020-08-04 22:08:26 -0400

    deleted some constants I don't use

--- src/main/java/io/zipcoder/casino/card/utilities/CeeLoConstant.java
+++ src/main/java/io/zipcoder/casino/card/utilities/CeeLoConstant.java
@@ -8,13 +8,11 @@ public class CeeLoConstant {
     public static final String CURRENT_POT = "The current pot is this amount: ";
     public static final String RULES_PROMPT = "Would you like to see the rules?";
     public static final String ROLL_DICE_PROMPT = "Press enter to roll the dice.";
-    public static final String DEPOSIT_POT = "This amount will be deposited to your account: ";
     public static final String UPDATED_BALANCE= "Your new balance is:";
     public static final String SCORE = "Your score is: ";
     public static final String BYE_PROMPT = "Thank you for playing CeeLo!";
     public static final String ASK_TO_REPLAY = "Would you like to play again? yes/no";
     public static final String YES = "yes";
-    public static final String NO = "no";
     public static final String PLAYER = "Player";
     public static final String HOUSE = "House";
     public static final String PLAYER_WINS = "The Player wins";
